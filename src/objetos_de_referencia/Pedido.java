6acdcbe7ffd9951d8cb06eb108853f3ed4d4f37b
weslleymberg <weslleym.lisboa@gmail.com>
2013-02-06 10:44:36 -0200

    Terminada a classe Pedido

--- src/objetos_de_referencia/Pedido.java
+++ src/objetos_de_referencia/Pedido.java
@@ -1,14 +1,29 @@
 package objetos_de_referencia;
 
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Pedido extends ObjetoDominio {
+	
+	public static int STATUS_EMITIDO = 0;
+	public static int STATUS_CONFIRMADO = 1;
+	public static int STATUS_ENTREGANDO = 2;
+	public static int STATUS_CONCLUIDO = 3;
+	public static int STATUS_CANCELADO = 4;
 	
 	private Cliente cliente;
 	private String enderecoEntrega;
 	private BigDecimal valorFreteKg;
-	HashMap<Produto, Item> itens = new HashMap<Produto, Item>();
+	private Date dataCancelamento;
+	private Date dataConclusao;
+	private Date dataCriacao = new Date();
+	private Date dataConfirmacao;
+	private int status = 0;
+	private long codigoProximoItem = 0;
+	Map<Produto, Item> itens = new HashMap<Produto, Item>();
 
 	public Pedido(long codigo, Cliente cliente, String enderecoEntrega, BigDecimal valorFreteKg) {
 		super(codigo);
@@ -34,41 +49,106 @@ public class Pedido extends ObjetoDominio {
 		this.valorFreteKg = valorFreteKg;
 	}
 
+	public Date getDataCancelamento() {
+		return (Date) dataCancelamento.clone();
+	}
+
+	public Date getDataConclusao() {
+		return (Date) dataConclusao.clone();
+	}
+
+	public Date getDataCriacao() {
+		return (Date) dataCriacao.clone();
+	}
+
+	public Date getDataConfirmacao() {
+		return (Date) dataConfirmacao.clone();
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
 	public Cliente getCliente() {
 		return cliente;
 	}
 	
+	public Iterator<Item> getTodosItens() {
+		return this.itens.values().iterator();
+	}
+	
 	public Item getItem(Produto produto) {
-		return itens.get(produto);
+		return this.itens.get(produto);
 	}
 	
 	public void atualizarItem(Produto produto, BigDecimal qtde) {
 		if (qtde.compareTo(new BigDecimal("0")) <= 0){
-			itens.remove(produto);
+			this.itens.remove(produto);
 		} else {
-			Item item = new Item(1234, produto, qtde);
-			itens.put(produto, item);
+			Item item = new Item(this.codigoProximoItem, produto, qtde);
+			this.itens.put(produto, item);
+			this.codigoProximoItem += 1;
+		}
+	}
+	
+	public void confirmar() {
+		Iterator<Item> iterator = this.getTodosItens();
+		while (iterator.hasNext()) {
+			iterator.next().confirmar();
+		}
+		this.dataConfirmacao = new Date();
+		this.status = Pedido.STATUS_CONFIRMADO;
+	}
+	
+	public void cancelar() {
+		if (this.getStatus() == Pedido.STATUS_CONCLUIDO) {
+			throw new IllegalStateException("Impossível cancelar! Pedido já foi entregue.");
+		} else {
+			if (this.getStatus() == Pedido.STATUS_ENTREGANDO) {
+				throw new IllegalStateException("Impossivel cancelar! Produto já está sendo entregue.");
+			} else {
+				this.status = Pedido.STATUS_CANCELADO;
+				this.dataCancelamento = new Date();
+			}
+		}
+	}
+	
+	public void entregar(Produto produto, BigDecimal qtde){
+		if (this.getStatus() == Pedido.STATUS_CONCLUIDO){
+			throw new IllegalArgumentException("Este pedido já foi entregue");
+		} else {
+			this.itens.get(produto).setQtdeEntregue(qtde);
+			for (Item item: itens.values()){
+				if (!(item.concluido())) {
+					this.status = Pedido.STATUS_ENTREGANDO;
+					break;
+				} else {
+					this.status = Pedido.STATUS_CONCLUIDO;
+				}
+			}
 		}
 	}
 	
 	public BigDecimal calcularTotalItens() {
 		BigDecimal totalItens = new BigDecimal("0");
-		for (Item item: itens.values()) {
+		for (Item item: this.itens.values()) {
 			totalItens = totalItens.add(item.calcularTotal());
 		}
 		return totalItens;
-	}
+	}	
 	
 	public BigDecimal calcularFrete() {
 		BigDecimal totalFrete = new BigDecimal("0");
-		for (Item item: itens.values()) {
+		for (Item item: this.itens.values()) {
 			totalFrete = totalFrete.add(item.calcularFrete(this.getValorFreteKg()));
 		}
 		return totalFrete;
 	}
 	
 	public BigDecimal calcularTotalGeral() {
-		return this.calcularTotalItens().add(this.calcularFrete());
+		BigDecimal totalGeral = new BigDecimal("0");
+		totalGeral = this.calcularTotalItens().add(this.calcularFrete());
+		return totalGeral.subtract(totalGeral.multiply(this.getCliente().getPorcentoDesconto())).setScale(2);
 	}
 
 }
--- srcTest/objetos_de_referencia/PedidoTeste.java
+++ srcTest/objetos_de_referencia/PedidoTeste.java
@@ -16,16 +16,16 @@ public class PedidoTeste {
 	
 	private Pedido pedido;
 	private Cliente cliente;
-	private DocIdentificador cpf;
-	private Produto produto;
-	private Produto produto2;
-	private Produto produto3;
-	private Produto kit;
+	private Cpf cpf;
+	private ProdutoSimples produto;
+	private ProdutoSimples produto2;
+	private ProdutoSimples produto3;
+	private Kit kit;
 
 	@Before
 	public void setUp() throws Exception {
 		cpf = new Cpf("12345678901");
-		cliente = new ClientePF(1231, "Weslleymberg", "Campos dos Goytacazes - RJ", "(22)9931-2376", new BigDecimal("0.1"), (Cpf) cpf);
+		cliente = new ClientePF(1231, "Weslleymberg", "Campos dos Goytacazes - RJ", "(22)9931-2376", new BigDecimal("0.1"), cpf);
 		pedido = new Pedido(1232, cliente, "Campos dos Goytacazes - RJ", new BigDecimal("3.00"));
 		produto = new ProdutoSimples(1235, "Xperia Neo", "Eletrônicos", new BigDecimal("0.30"), new BigDecimal("800.00"));
 		kit = new Kit(1235, "Acessórios", "Acessórios Sony", new BigDecimal("0"), new BigDecimal("0.2"));
@@ -73,17 +73,64 @@ public class PedidoTeste {
 	public void deve_calcular_o_frete() {
 		pedido.atualizarItem(produto, new BigDecimal("1"));
 		pedido.atualizarItem(kit, new BigDecimal("1"));
-		((Kit) kit).adicionarFilho(produto2);
-		((Kit) kit).adicionarFilho(produto3);
+		kit.adicionarFilho(produto2);
+		kit.adicionarFilho(produto3);
 		assertThat(pedido.calcularFrete(), is(new BigDecimal("1.50")));
 	}
 	
+	@Test
 	public void deve_calcular_o_valor_total() {
 		pedido.atualizarItem(produto, new BigDecimal("1"));
 		pedido.atualizarItem(kit, new BigDecimal("1"));
-		((Kit) kit).adicionarFilho(produto2);
-		((Kit) kit).adicionarFilho(produto3);
-		assertThat(pedido.calcularTotalGeral(), is(new BigDecimal("1041.50")));
+		kit.adicionarFilho(produto2);
+		kit.adicionarFilho(produto3);
+		assertThat(pedido.calcularTotalGeral(), is(new BigDecimal("937.35")));
+	}
+	
+	@Test
+	public void deve_confirmar_o_pedido() {
+		pedido.atualizarItem(produto, new BigDecimal("1"));
+		pedido.atualizarItem(kit, new BigDecimal("1"));
+		kit.adicionarFilho(produto2);
+		kit.adicionarFilho(produto3);
+		pedido.confirmar();
+		assertThat(pedido.getStatus(), is(Pedido.STATUS_CONFIRMADO));
+	}
+	
+	@Test
+	public void deve_cancelar_o_pedido() {
+		pedido.atualizarItem(produto, new BigDecimal("1"));
+		pedido.atualizarItem(kit, new BigDecimal("1"));
+		kit.adicionarFilho(produto2);
+		kit.adicionarFilho(produto3);
+		assertThat(pedido.getStatus(), is(Pedido.STATUS_EMITIDO));
+		pedido.cancelar();
+		assertThat(pedido.getStatus(), is(Pedido.STATUS_CANCELADO));
+	}
+	
+	@Test
+	public void deve_entregar_o_pedido() {
+		pedido.atualizarItem(produto, new BigDecimal("1"));
+		pedido.atualizarItem(kit, new BigDecimal("1"));
+		kit.adicionarFilho(produto2);
+		kit.adicionarFilho(produto3);
+		pedido.confirmar();
+		pedido.entregar(produto, new BigDecimal("1"));
+		assertThat(pedido.getStatus(), is(Pedido.STATUS_ENTREGANDO));
+		pedido.entregar(kit, new BigDecimal("1"));
+		assertThat(pedido.getStatus(), is(Pedido.STATUS_CONCLUIDO));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void deve_retornar_produto_concluido() {
+		pedido.atualizarItem(produto, new BigDecimal("1"));
+		pedido.atualizarItem(kit, new BigDecimal("1"));
+		kit.adicionarFilho(produto2);
+		kit.adicionarFilho(produto3);
+		pedido.confirmar();
+		pedido.entregar(produto, new BigDecimal("1"));
+		assertThat(pedido.getStatus(), is(Pedido.STATUS_ENTREGANDO));
+		pedido.entregar(produto, new BigDecimal("1"));
 	}
 
 }
